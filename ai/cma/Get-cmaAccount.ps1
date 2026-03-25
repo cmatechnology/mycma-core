e8a551b538838f820c7581450934660221b35cc5
David Baker <dbaker@cmaaugusta.com>
2026-03-25 10:59:06 -0400

    ai testing

---
+++ ai/cma/Get-cmaAccount.ps1
@@ -0,0 +1,24 @@
+<#
+.SYNOPSIS
+Get the list of system ids for a customer account
+
+.DESCRIPTION
+Automated system compile the unique ids used to identify each customer in each of the third party systems.
+The "Account" record keeps each of these ids for a customer.
+
+.PARAMETER Code
+Customer Account Code
+
+#>
+
+Function Get-cmaAccount {
+    param (
+        [Parameter(Mandatory=$false)] [String] $Code
+    )
+
+    $url =  "/api/cma/account?code=$Code"
+
+    $response = Invoke-cmaRequest -Method Get -Uri $url 
+
+    return $response
+}
